703f006b9ae4897c0ec80923a0c3f40b7b8b901d
Sivakar <sivakar12@outlook.com>
2016-04-16 20:13:56 +0530

    Add routes for Suppliers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/suppliers', 'SuppliersController@viewSuppliers');
+
+Route::get('/suppliers/{supplier}', 'SuppliersController@viewSupplier');
