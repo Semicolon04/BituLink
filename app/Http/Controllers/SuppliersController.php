fe916a0e0d15d03a56d30a105ece23f65300e3a0
Sivakar <sivakar12@outlook.com>
2016-04-16 20:11:06 +0530

    Create controller for Supplier

---
+++ app/Http/Controllers/SuppliersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class SuppliersController extends Controller
+{
+    public function viewSuppliers() {
+			
+		}
+		public function viewSupplier(Supplier $supplier) {
+			
+		}
+		public function createSupplier($request) {
+			
+		}
+}
