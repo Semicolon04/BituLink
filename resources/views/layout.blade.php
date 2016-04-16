15c3902164535950ae020cfc9f132317e7c2a3af
Sivakar <sivakar12@outlook.com>
2016-04-16 19:56:58 +0530

    Create a basic layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bitulink Dealings</title>
+		<link rel="stylesheet" href="/bootstrap.min.css">
+	</head>
+	<body>
+		<header>
+			<h1>Bitulink Dealings</h1>
+		</header>
+		@yield('content')
+		<footer></footer>
+	</body>
+</html>
